9b5a6e7abc658c3e66ba223da77fae2df8c50305
karolmajek <karolmajek@gmail.com>
2015-05-03 13:15:27 +0200

    batch files
    batch files for experiments from article

---
+++ data/simCPU.bat
@@ -0,0 +1,114 @@
+SensorSimRT.exe --cpu 1 --rays rays\conference_3Dlms100.binary --mesh scenes\conference.binary --width 541 --height 250 --image images\conf_3dlms100_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\conference_hdl32.binary --mesh scenes\conference.binary --width 2187 --height 32 --image images\conf_hdl32_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\conference_hdl64.binary --mesh scenes\conference.binary --width 4000 --height 64 --image images\conf_hdl64_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\conference_kinect1.binary --mesh scenes\conference.binary --width 320 --height 240 --image images\conf_kinect1_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\conference_kinect2.binary --mesh scenes\conference.binary --width 512 --height 424 --image images\conf_kinect2_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\conference_lms100.binary --mesh scenes\conference.binary --width 541 --height 1 --image images\conf_lms100_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\conference_vlp16.binary --mesh scenes\conference.binary --width 1875 --height 16 --image images\conf_vlp16_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\conference_zf.binary --mesh scenes\conference.binary --width 5000 --height 5000 --image images\conf_zf_CPU_1.pgm
+
+SensorSimRT.exe --cpu 1 --rays rays\sm_3Dlms100.binary --mesh scenes\san_miguel.binary --width 541 --height 250 --image images\sm_3dlms100_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\sm_hdl32.binary --mesh scenes\san_miguel.binary --width 2187 --height 32 --image images\sm_hdl32_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\sm_hdl64.binary --mesh scenes\san_miguel.binary --width 4000 --height 64 --image images\sm_hdl64_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\sm_kinect1.binary --mesh scenes\san_miguel.binary --width 320 --height 240 --image images\sm_kinect1_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\sm_kinect2.binary --mesh scenes\san_miguel.binary --width 512 --height 424 --image images\sm_kinect2_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\sm_lms100.binary --mesh scenes\san_miguel.binary --width 541 --height 1 --image images\sm_lms100_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\sm_vlp16.binary --mesh scenes\san_miguel.binary --width 1875 --height 16 --image images\sm_vlp16_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\sm_zf.binary --mesh scenes\san_miguel.binary --width 5000 --height 5000 --image images\sm_zf_CPU_1.pgm
+
+SensorSimRT.exe --cpu 1 --rays rays\pp_3Dlms100.binary --mesh scenes\powerplant.binary --width 541 --height 250 --image images\pp_3dlms100_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\pp_hdl32.binary --mesh scenes\powerplant.binary --width 2187 --height 32 --image images\pp_hdl32_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\pp_hdl64.binary --mesh scenes\powerplant.binary --width 4000 --height 64 --image images\pp_hdl64_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\pp_kinect1.binary --mesh scenes\powerplant.binary --width 320 --height 240 --image images\pp_kinect1_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\pp_kinect2.binary --mesh scenes\powerplant.binary --width 512 --height 424 --image images\pp_kinect2_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\pp_lms100.binary --mesh scenes\powerplant.binary --width 541 --height 1 --image images\pp_lms100_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\pp_vlp16.binary --mesh scenes\powerplant.binary --width 1875 --height 16 --image images\pp_vlp16_CPU_1.pgm
+SensorSimRT.exe --cpu 1 --rays rays\pp_zf.binary --mesh scenes\powerplant.binary --width 5000 --height 5000 --image images\pp_zf_CPU_1.pgm
+
+
+SensorSimRT.exe --cpu 2 --rays rays\conference_3Dlms100.binary --mesh scenes\conference.binary --width 541 --height 250 --image images\conf_3dlms100_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\conference_hdl32.binary --mesh scenes\conference.binary --width 2187 --height 32 --image images\conf_hdl32_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\conference_hdl64.binary --mesh scenes\conference.binary --width 4000 --height 64 --image images\conf_hdl64_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\conference_kinect1.binary --mesh scenes\conference.binary --width 320 --height 240 --image images\conf_kinect1_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\conference_kinect2.binary --mesh scenes\conference.binary --width 512 --height 424 --image images\conf_kinect2_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\conference_lms100.binary --mesh scenes\conference.binary --width 541 --height 2 --image images\conf_lms100_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\conference_vlp16.binary --mesh scenes\conference.binary --width 1875 --height 16 --image images\conf_vlp16_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\conference_zf.binary --mesh scenes\conference.binary --width 5000 --height 5000 --image images\conf_zf_CPU_2.pgm
+
+SensorSimRT.exe --cpu 2 --rays rays\sm_3Dlms100.binary --mesh scenes\san_miguel.binary --width 541 --height 250 --image images\sm_3dlms100_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\sm_hdl32.binary --mesh scenes\san_miguel.binary --width 2187 --height 32 --image images\sm_hdl32_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\sm_hdl64.binary --mesh scenes\san_miguel.binary --width 4000 --height 64 --image images\sm_hdl64_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\sm_kinect1.binary --mesh scenes\san_miguel.binary --width 320 --height 240 --image images\sm_kinect1_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\sm_kinect2.binary --mesh scenes\san_miguel.binary --width 512 --height 424 --image images\sm_kinect2_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\sm_lms100.binary --mesh scenes\san_miguel.binary --width 541 --height 2 --image images\sm_lms100_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\sm_vlp16.binary --mesh scenes\san_miguel.binary --width 1875 --height 16 --image images\sm_vlp16_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\sm_zf.binary --mesh scenes\san_miguel.binary --width 5000 --height 5000 --image images\sm_zf_CPU_2.pgm
+
+SensorSimRT.exe --cpu 2 --rays rays\pp_3Dlms100.binary --mesh scenes\powerplant.binary --width 541 --height 250 --image images\pp_3dlms100_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\pp_hdl32.binary --mesh scenes\powerplant.binary --width 2187 --height 32 --image images\pp_hdl32_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\pp_hdl64.binary --mesh scenes\powerplant.binary --width 4000 --height 64 --image images\pp_hdl64_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\pp_kinect1.binary --mesh scenes\powerplant.binary --width 320 --height 240 --image images\pp_kinect1_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\pp_kinect2.binary --mesh scenes\powerplant.binary --width 512 --height 424 --image images\pp_kinect2_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\pp_lms100.binary --mesh scenes\powerplant.binary --width 541 --height 2 --image images\pp_lms100_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\pp_vlp16.binary --mesh scenes\powerplant.binary --width 1875 --height 16 --image images\pp_vlp16_CPU_2.pgm
+SensorSimRT.exe --cpu 2 --rays rays\pp_zf.binary --mesh scenes\powerplant.binary --width 5000 --height 5000 --image images\pp_zf_CPU_2.pgm
+
+
+SensorSimRT.exe --cpu 4 --rays rays\conference_3Dlms100.binary --mesh scenes\conference.binary --width 541 --height 250 --image images\conf_3dlms100_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\conference_hdl32.binary --mesh scenes\conference.binary --width 2187 --height 32 --image images\conf_hdl32_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\conference_hdl64.binary --mesh scenes\conference.binary --width 4000 --height 64 --image images\conf_hdl64_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\conference_kinect1.binary --mesh scenes\conference.binary --width 320 --height 240 --image images\conf_kinect1_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\conference_kinect2.binary --mesh scenes\conference.binary --width 512 --height 424 --image images\conf_kinect2_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\conference_lms100.binary --mesh scenes\conference.binary --width 541 --height 4 --image images\conf_lms100_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\conference_vlp16.binary --mesh scenes\conference.binary --width 1875 --height 16 --image images\conf_vlp16_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\conference_zf.binary --mesh scenes\conference.binary --width 5000 --height 5000 --image images\conf_zf_CPU_4.pgm
+
+SensorSimRT.exe --cpu 4 --rays rays\sm_3Dlms100.binary --mesh scenes\san_miguel.binary --width 541 --height 250 --image images\sm_3dlms100_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\sm_hdl32.binary --mesh scenes\san_miguel.binary --width 2187 --height 32 --image images\sm_hdl32_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\sm_hdl64.binary --mesh scenes\san_miguel.binary --width 4000 --height 64 --image images\sm_hdl64_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\sm_kinect1.binary --mesh scenes\san_miguel.binary --width 320 --height 240 --image images\sm_kinect1_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\sm_kinect2.binary --mesh scenes\san_miguel.binary --width 512 --height 424 --image images\sm_kinect2_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\sm_lms100.binary --mesh scenes\san_miguel.binary --width 541 --height 4 --image images\sm_lms100_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\sm_vlp16.binary --mesh scenes\san_miguel.binary --width 1875 --height 16 --image images\sm_vlp16_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\sm_zf.binary --mesh scenes\san_miguel.binary --width 5000 --height 5000 --image images\sm_zf_CPU_4.pgm
+
+SensorSimRT.exe --cpu 4 --rays rays\pp_3Dlms100.binary --mesh scenes\powerplant.binary --width 541 --height 250 --image images\pp_3dlms100_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\pp_hdl32.binary --mesh scenes\powerplant.binary --width 2187 --height 32 --image images\pp_hdl32_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\pp_hdl64.binary --mesh scenes\powerplant.binary --width 4000 --height 64 --image images\pp_hdl64_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\pp_kinect1.binary --mesh scenes\powerplant.binary --width 320 --height 240 --image images\pp_kinect1_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\pp_kinect2.binary --mesh scenes\powerplant.binary --width 512 --height 424 --image images\pp_kinect2_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\pp_lms100.binary --mesh scenes\powerplant.binary --width 541 --height 4 --image images\pp_lms100_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\pp_vlp16.binary --mesh scenes\powerplant.binary --width 1875 --height 16 --image images\pp_vlp16_CPU_4.pgm
+SensorSimRT.exe --cpu 4 --rays rays\pp_zf.binary --mesh scenes\powerplant.binary --width 5000 --height 5000 --image images\pp_zf_CPU_4.pgm
+
+
+SensorSimRT.exe --cpu 8 --rays rays\conference_3Dlms100.binary --mesh scenes\conference.binary --width 541 --height 250 --image images\conf_3dlms100_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\conference_hdl32.binary --mesh scenes\conference.binary --width 2187 --height 32 --image images\conf_hdl32_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\conference_hdl64.binary --mesh scenes\conference.binary --width 4000 --height 64 --image images\conf_hdl64_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\conference_kinect1.binary --mesh scenes\conference.binary --width 320 --height 240 --image images\conf_kinect1_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\conference_kinect2.binary --mesh scenes\conference.binary --width 512 --height 424 --image images\conf_kinect2_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\conference_lms100.binary --mesh scenes\conference.binary --width 541 --height 8 --image images\conf_lms100_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\conference_vlp16.binary --mesh scenes\conference.binary --width 1875 --height 16 --image images\conf_vlp16_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\conference_zf.binary --mesh scenes\conference.binary --width 5000 --height 5000 --image images\conf_zf_CPU_8.pgm
+
+SensorSimRT.exe --cpu 8 --rays rays\sm_3Dlms100.binary --mesh scenes\san_miguel.binary --width 541 --height 250 --image images\sm_3dlms100_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\sm_hdl32.binary --mesh scenes\san_miguel.binary --width 2187 --height 32 --image images\sm_hdl32_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\sm_hdl64.binary --mesh scenes\san_miguel.binary --width 4000 --height 64 --image images\sm_hdl64_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\sm_kinect1.binary --mesh scenes\san_miguel.binary --width 320 --height 240 --image images\sm_kinect1_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\sm_kinect2.binary --mesh scenes\san_miguel.binary --width 512 --height 424 --image images\sm_kinect2_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\sm_lms100.binary --mesh scenes\san_miguel.binary --width 541 --height 8 --image images\sm_lms100_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\sm_vlp16.binary --mesh scenes\san_miguel.binary --width 1875 --height 16 --image images\sm_vlp16_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\sm_zf.binary --mesh scenes\san_miguel.binary --width 5000 --height 5000 --image images\sm_zf_CPU_8.pgm
+
+SensorSimRT.exe --cpu 8 --rays rays\pp_3Dlms100.binary --mesh scenes\powerplant.binary --width 541 --height 250 --image images\pp_3dlms100_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\pp_hdl32.binary --mesh scenes\powerplant.binary --width 2187 --height 32 --image images\pp_hdl32_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\pp_hdl64.binary --mesh scenes\powerplant.binary --width 4000 --height 64 --image images\pp_hdl64_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\pp_kinect1.binary --mesh scenes\powerplant.binary --width 320 --height 240 --image images\pp_kinect1_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\pp_kinect2.binary --mesh scenes\powerplant.binary --width 512 --height 424 --image images\pp_kinect2_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\pp_lms100.binary --mesh scenes\powerplant.binary --width 541 --height 8 --image images\pp_lms100_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\pp_vlp16.binary --mesh scenes\powerplant.binary --width 1875 --height 16 --image images\pp_vlp16_CPU_8.pgm
+SensorSimRT.exe --cpu 8 --rays rays\pp_zf.binary --mesh scenes\powerplant.binary --width 5000 --height 5000 --image images\pp_zf_CPU_8.pgm
+
+
+
+pause
---
+++ data/simGPU.bat
@@ -0,0 +1,27 @@
+SensorSimRT.exe --rays rays\conference_3Dlms100.binary --mesh scenes\conference.binary --width 541 --height 250 --image images\conf_3dlms100.pgm
+SensorSimRT.exe --rays rays\conference_hdl32.binary --mesh scenes\conference.binary --width 2187 --height 32 --image images\conf_hdl32.pgm
+SensorSimRT.exe --rays rays\conference_hdl64.binary --mesh scenes\conference.binary --width 4000 --height 64 --image images\conf_hdl64.pgm
+SensorSimRT.exe --rays rays\conference_kinect1.binary --mesh scenes\conference.binary --width 320 --height 240 --image images\conf_kinect1.pgm
+SensorSimRT.exe --rays rays\conference_kinect2.binary --mesh scenes\conference.binary --width 512 --height 424 --image images\conf_kinect2.pgm
+SensorSimRT.exe --rays rays\conference_lms100.binary --mesh scenes\conference.binary --width 541 --height 1 --image images\conf_lms100.pgm
+SensorSimRT.exe --rays rays\conference_vlp16.binary --mesh scenes\conference.binary --width 1875 --height 16 --image images\conf_vlp16.pgm
+SensorSimRT.exe --rays rays\conference_zf.binary --mesh scenes\conference.binary --width 5000 --height 5000 --image images\conf_zf.pgm
+
+SensorSimRT.exe --rays rays\sm_3Dlms100.binary --mesh scenes\san_miguel.binary --width 541 --height 250 --image images\sm_3dlms100.pgm
+SensorSimRT.exe --rays rays\sm_hdl32.binary --mesh scenes\san_miguel.binary --width 2187 --height 32 --image images\sm_hdl32.pgm
+SensorSimRT.exe --rays rays\sm_hdl64.binary --mesh scenes\san_miguel.binary --width 4000 --height 64 --image images\sm_hdl64.pgm
+SensorSimRT.exe --rays rays\sm_kinect1.binary --mesh scenes\san_miguel.binary --width 320 --height 240 --image images\sm_kinect1.pgm
+SensorSimRT.exe --rays rays\sm_kinect2.binary --mesh scenes\san_miguel.binary --width 512 --height 424 --image images\sm_kinect2.pgm
+SensorSimRT.exe --rays rays\sm_lms100.binary --mesh scenes\san_miguel.binary --width 541 --height 1 --image images\sm_lms100.pgm
+SensorSimRT.exe --rays rays\sm_vlp16.binary --mesh scenes\san_miguel.binary --width 1875 --height 16 --image images\sm_vlp16.pgm
+SensorSimRT.exe --rays rays\sm_zf.binary --mesh scenes\san_miguel.binary --width 5000 --height 5000 --image images\sm_zf.pgm
+
+SensorSimRT.exe --rays rays\pp_3Dlms100.binary --mesh scenes\powerplant.binary --width 541 --height 250 --image images\pp_3dlms100.pgm
+SensorSimRT.exe --rays rays\pp_hdl32.binary --mesh scenes\powerplant.binary --width 2187 --height 32 --image images\pp_hdl32.pgm
+SensorSimRT.exe --rays rays\pp_hdl64.binary --mesh scenes\powerplant.binary --width 4000 --height 64 --image images\pp_hdl64.pgm
+SensorSimRT.exe --rays rays\pp_kinect1.binary --mesh scenes\powerplant.binary --width 320 --height 240 --image images\pp_kinect1.pgm
+SensorSimRT.exe --rays rays\pp_kinect2.binary --mesh scenes\powerplant.binary --width 512 --height 424 --image images\pp_kinect2.pgm
+SensorSimRT.exe --rays rays\pp_lms100.binary --mesh scenes\powerplant.binary --width 541 --height 1 --image images\pp_lms100.pgm
+SensorSimRT.exe --rays rays\pp_vlp16.binary --mesh scenes\powerplant.binary --width 1875 --height 16 --image images\pp_vlp16.pgm
+SensorSimRT.exe --rays rays\pp_zf.binary --mesh scenes\powerplant.binary --width 5000 --height 5000 --image images\pp_zf.pgm
+pause
